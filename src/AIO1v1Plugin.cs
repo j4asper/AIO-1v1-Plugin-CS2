45e5b587ee7ac98e123f2281c1caabbbabded337
Jasper Christiansen <jasper@jazper.dk>
2024-04-11 10:27:31 +0200

    Update load method

--- src/AIO1v1Plugin.cs
+++ src/AIO1v1Plugin.cs
@@ -1,18 +1,30 @@
-﻿using CounterStrikeSharp.API.Core;
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
 
 namespace AIO1v1Plugin;
 
-public class AIO1v1Plugin : BasePlugin
+public partial class AIO1v1Plugin : BasePlugin
 {
     public override string ModuleName => "AIO1v1Plugin";
     public override string ModuleVersion => "1.0";
     public override string ModuleDescription => "All-in-One 1v1 Plugin for Counter-Strike 2";
-	public override string ModuleAuthor => "j4asper";
+    public override string ModuleAuthor => "j4asper";
 
     public override void Load(bool hotReload)
     {
-        Console.WriteLine("AIO 1v1 Plugin by j4asper");
-        Console.WriteLine($"Plugin Version: {ModuleVersion}");
-        Console.WriteLine("https://github.com/j4asper/AIO-1v1-Plugin-CS2");
+        string prefix = $"[{ModuleName}] ";
+
+        string[] messages = [
+            $"{ModuleName} by {ModuleAuthor}",
+            $"Plugin Version: {ModuleVersion}",
+            "Source: https://github.com/j4asper/AIO-1v1-Plugin-CS2"
+        ];
+
+        foreach (string message in messages)
+        {
+            Console.WriteLine(prefix + message);
+        }
     }
 }
