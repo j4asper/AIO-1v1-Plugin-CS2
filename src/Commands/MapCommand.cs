95071cdf0cfc3ff85e88d4a274b9722b88eab62f
Jasper Christiansen <jasper@jazper.dk>
2024-04-15 00:15:15 +0200

    Added Map command

--- src/Commands/MapCommand.cs
+++ src/Commands/MapCommand.cs
@@ -1,3 +1,4 @@
+using System.Xml.Schema;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
@@ -8,33 +9,18 @@ namespace AIO1v1Plugin;
 public partial class AIO1v1Plugin
 {
     [ConsoleCommand("css_map", "Change the current map using this command.")]
-    public void OnMapCommand(CCSPlayerController? player, CommandInfo commandInfo)
+    [CommandHelper(minArgs: 1, usage: "[map name]", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+    public void OnMapCommand(CCSPlayerController? _player, CommandInfo commandInfo)
     {
-        // The first argument is the command name, in this case "css_hello".
-        commandInfo.GetArg(0); // css_hello
+        string mapName = commandInfo.GetArg(1);
 
-        // The second argument is the first argument passed to the command, in this case "name".
-        // The `minArgs` helper parameter is used to ensure that the second argument is present.
-        //var name = commandInfo.GetArg(1);
-
-        string[] maps = Server.GetMapList();
-
-        commandInfo.ReplyToCommand(string.Join("\n", maps));
-    }
-
-
-    // Commands can also be registered using the `Command` attribute.
-    [ConsoleCommand("css_hello", "Responds to the caller with \"pong\"")]
-    // The `CommandHelper` attribute can be used to provide additional information about the command.
-    public void OnHelloCommand(CCSPlayerController? player, CommandInfo commandInfo)
-    {
-        // The first argument is the command name, in this case "css_hello".
-        //commandInfo.GetArg(0); // css_hello
-
-        // The second argument is the first argument passed to the command, in this case "name".
-        // The `minArgs` helper parameter is used to ensure that the second argument is present.
-        //var name = commandInfo.GetArg(1);
-
-        commandInfo.ReplyToCommand("Hello");
+        if(Server.IsMapValid(mapName))
+        {
+            commandInfo.ReplyToCommand($"Map: \"{mapName}\" is not a valid map.");
+        }
+        else
+        {
+            Server.ExecuteCommand($"map de_{mapName}");
+        }
     }
 }
